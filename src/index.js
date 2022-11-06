99f557943a9a1321c30dd240a2f70180ffed9d4d
albert patterson <apatterson189@gmail.com>
2022-11-06 12:41:29 -0600

    initial working with html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import './style.css';
+import shopkinImg from './images/shopkin.png';
 import { log } from './util/util.js';
 
 console.log('hello world');
 log();
+
+const img = document.createElement('img');
+img.src = shopkinImg;
+document.body.appendChild(img);
